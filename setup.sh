180f04a096be1c50de43acbe9b9aea2834ebc99a
GeekCmore <geekcmore@163.com>
2024-08-30 00:42:49 +0800

    Fix: Add setup of patchelf

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 echo "Start setup!"
 pip install -r requirements.txt
+sudo apt-get install patchelf
 mkdir -p ~/.config/cpwn
 cp config.json ~/.config/cpwn/
 cp template.py ~/.config/cpwn/exp_template.py
